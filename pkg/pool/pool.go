4b1edf4cd47709490903c3fd1a6ce379a3bb99d2
Pro100x3mal <Pro100x3mal@gmail.com>
2025-11-04 19:32:40 +0300

    feat(pool): introduce generic `Pool` implementation with resettable objects and tests

---
+++ pkg/pool/pool.go
@@ -0,0 +1,31 @@
+package pool
+
+import "sync"
+
+type Resetter interface {
+	Reset()
+}
+
+type Pool[T Resetter] struct {
+	pool sync.Pool
+}
+
+func New[T Resetter](newFunc func() T) *Pool[T] {
+	return &Pool[T]{
+		pool: sync.Pool{
+			New: func() interface{} {
+				return newFunc()
+			},
+		},
+	}
+}
+
+func (p *Pool[T]) Get() T {
+	obj := p.pool.Get()
+	return obj.(T)
+}
+
+func (p *Pool[T]) Put(obj T) {
+	obj.Reset()
+	p.pool.Put(obj)
+}
